ea6f3ec1e5910c91db8f76e5ab48c7a0973bcf90
Breathe <704340916@qq.com>
2023-10-11 12:36:59 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import MotherDay from "./components/MotherDay.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://electron-vite.github.io" target="_blank">
-      <img src="/electron-vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="menus">
+    <div class="menus-item" @click="setTray">最小化</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <!-- <MotherDay :option="{ flakeCount: 30 }" /> -->
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+// import { Menu, Tray } from 'electron'
+const setTray = () => {
+  console.log('\x1b[38;2;0;151;255m%c%s\x1b[0m', 'color:#0097ff;padding:16px 0;', `------->Breathe:`)
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+<style scoped lang="scss">
+.menus {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .menus-item {
+    padding: 8px 20px;
+    background-color: #2b2b2b;
+    border-radius: 12px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+      color: #0097ff;
+    }
+  }
 }
 </style>
